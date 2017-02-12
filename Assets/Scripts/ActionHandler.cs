8f6185d095a7fac314fd4812fda46d9ee31bdc75
Adam Mitchell <Adam Mitchell>
2017-02-11 19:45:55 -0600

    Added the ability to hover over items and highlight them.

---
+++ Assets/Scripts/ActionHandler.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+public class ActionHandler : Selectable
+{
+    public Selectable selectObject;
+    public SpriteRenderer spriteRenderer;
+
+    private bool isMousedOver;
+
+    protected override void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    public void OnSelect()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (isMousedOver)
+        {
+            spriteRenderer.material = (Material) AssetDatabase.LoadAssetAtPath("Assets/Materials/HighlightMat.mat", typeof(Material));
+        } else if (!isMousedOver)
+        {
+            spriteRenderer.material = (Material)AssetDatabase.LoadAssetAtPath("Assets/Materials/PlainMat.mat", typeof(Material));
+        }
+    }
+
+    public void OnMouseOver()
+    {
+        isMousedOver = true;
+        print("Iss do something!");
+    }
+
+    private void OnMouseExit()
+    {
+        isMousedOver = false;
+    }
+}
+
---
+++ Assets/Scripts/SceneItem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneItem : MonoBehaviour {
+
+    public String Name;
+    public String Description;
+
+    public Dictionary<String, SceneItem> itemSocket;  //dictionary<dictionary> itemSocket
+    public String keyValue;
+
+
+    //public override void OnSelected()
+    //{
+
+    //}
+
+    bool addTwinItem(SceneItem Item)
+    {
+		
+		// Check the value of the item to see if it's the right \\corresponding value
+
+        if (Item.keyValue == keyValue)
+        {
+            itemSocket.Add(Item.keyValue, Item);
+
+        }
+        else
+        {
+            return false;
+
+        }
+
+        return true; //check tutorial for code logic that checks for \\success or failure on an if statement
+
+    }
+
+    void GiveKeyValue(String givenKeyValue)
+    {
+        keyValue = givenKeyValue;
+    }
+
+}
