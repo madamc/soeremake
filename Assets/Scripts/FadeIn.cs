2556d2e383529ca8267b79f49a611340c183a87b
Peter <petemit>
2017-03-14 20:31:44 -0500

    made the fade in work

--- Assets/Scripts/FadeIn.cs
+++ Assets/Scripts/FadeIn.cs
@@ -5,23 +5,28 @@ using UnityEngine;
 public class FadeIn : MonoBehaviour {
 
     public bool isFadingIn=false;
-    public float alphamax = 255f;
+    public float alphamax = 1f;
     private float currentalpha = 0.0f;
+    public float fadespeed = 1f;
 	// Use this for initialization
 	void Start () {
         Color currentcolor = gameObject.GetComponent<SpriteRenderer>().color;
         currentcolor = new Color(currentcolor.r, currentcolor.g, currentcolor.b, 0.0f);
+        gameObject.GetComponent<SpriteRenderer>().color = currentcolor;
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if (isFadingIn ==true &&currentalpha!= alphamax) {
+        if (isFadingIn ==true &&currentalpha <= alphamax) {
             Color currentcolor = gameObject.GetComponent<SpriteRenderer>().color;
             currentalpha = currentcolor.a;
-            float nextalpha = currentalpha + Time.deltaTime;
+            float nextalpha = currentalpha + Time.deltaTime*fadespeed;
+            Debug.Log(currentalpha);
+           
 
             currentcolor = new Color(currentcolor.r, currentcolor.g, currentcolor.b, nextalpha);
+            gameObject.GetComponent<SpriteRenderer>().color = currentcolor;
         }
 	}
 }
